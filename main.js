840d9486cc615e9b2f599ef612de529b6ee14b22
Willian Dias <diaswillian75@gmail.com>
2022-08-17 20:23:09 -0300

    ES6

--- main.js
+++ main.js
@@ -1,65 +1,37 @@
-// Exemplo de classe com herança
+const arr = [1,2,3,4,5,6,,8,9]
 
-class List{
-    constructor(){
-        this.data = []
-    }
-    add(data){
-        this.data.push(data)
-        console.log(this.data)
-    }
+/*const newArr = arr.map(function(item){
+     return item * 2;
+})
+
+console.log(newArr)
+
+const soma = arr.reduce(function(total, next){
+    return total + next;
+})
+
+console.log(soma)
+
+const filter = arr.filter(function(item){ //filtra os elementos
+    return item %2 === 0; //Quando quer estritamente iguala a esse numero
+})
+
+console.log(filter) 
+
+const find = arr.find(function(item){//encontra exatamente uma opção solicitada
+    return item ===3;
+})
+
+console.log(find)
+
+const newArr = arr.map(item => item * 2)
+console.log(newArr)
+
+const test = () => [1,2,3]
+console.log (test)*/
+
+function soma (a,b){
+    return a+b;
 }
 
-class ToList extends List{
-    constructor(){
-        super();
-        this.usuario = "lucas"
-    }
-    getUsuario(){
-        console.log(this.usuario)
-    }
-
-    static soma(a,b){
-        return a+b
-        
-    }
-
-}
-
-//instacia a classe
-var todo = new ToList();
-
-//adiciona ao array data a string "adicionei" cada vez que clica no botão com id = "novotodo"  e chama o método getUsuario
-document.getElementById("novotodo").onclick = function(){
-    todo.add("adicionei");
-    todo.getUsuario()
-}
-
-
-//Chama o método estático 
-ToList.soma(1,3)
-
-//variavel constante
-const user = 'lucas'
-//ao executar o código abaixo gera um erro, pois a variável é constante
-//user = 'joao'
-console.log(user)
-
-//No caso abaixo há alteração da propriedade, mas da estrutura do objeto.
-const usuario = {nome: 'lucas'}
-usuario.nome = "joao"
-console.log(usuario)
-
-//Variável let só é visível dentro do escopo da função
-function teste (x){
-    let y = 4
-    if (x > 2){
-        y =6
-        console.log(x,y)
-    }
-}
-
-console.log(teste(10))
-
-//O código abaixo gera um erro, pois a variável não é visível fora da função
-//console.log(y)
+console.log(soma(2,3))
